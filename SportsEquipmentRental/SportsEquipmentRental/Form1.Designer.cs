171ad4c72fcbbf7c09be9e1affc384ab3c07bed3
SeregaGG <serega.taraskin.1999@mail.ru>
2019-04-20 21:36:33 +0500

    Очепятка

--- SportsEquipmentRental/SportsEquipmentRental/Form1.Designer.cs
+++ SportsEquipmentRental/SportsEquipmentRental/Form1.Designer.cs
@@ -136,7 +136,7 @@
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(86, 13);
             this.label9.TabIndex = 17;
-            this.label9.Text = "Фото продовца";
+            this.label9.Text = "Фото продавца";
             // 
             // pictureBox1
             // 
